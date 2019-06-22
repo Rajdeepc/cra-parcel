540119104f25aea4b798e8d2b0180131731c1e6a
Rajdeepc <rajrock38@gmail.com>
2019-06-22 21:44:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class App extends Component {
+    render() {
+        return <div><h2>I am a React App with Parcel bundler</h2></div>;
+    }
+}
+export default App;
