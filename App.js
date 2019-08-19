68085e377a90be1c3f211ac86505dfe1b62d03d7
Rajdeepc <rajrock38@gmail.com>
2019-08-19 22:50:27 +0530

    parcel

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
 class App extends Component {
     render() {
-        return <div><h2>I am a React App with Parcel bundler</h2></div>;
+        return (
+            <Container>
+                <Row>
+                    <Col><img src="https://www.woolha.com/media/2018/09/using-parceljs-bundler-for-building-reactjs-application-1200x627.jpg" />
+                        <h4>Hi i am a React Application with Redux built with Parcel</h4>
+                        <h4>This package contains: React Bootstrap</h4>
+                    </Col>
+                </Row>
+            </Container>
+
+        )
     }
 }
 export default App;
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+/*
+ src/index.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './src/store';
+import App from './App';
+ReactDOM.render(
+    <Provider store={configureStore()}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
